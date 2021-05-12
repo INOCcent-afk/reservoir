d055481992885d9b779b17775ebe381c46bbd0dc
Dave Inoc <michael828inoc@gmail.com>
2021-05-12 17:17:55 +0800

    added working auth for github google facebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
-import "./App.css";
+import {
+  facebookProvider,
+  githubProvider,
+  googleProvider,
+} from "./config/authMethod";
+import socialMediaAuth from "./service/auth";
 
 function App() {
+  const handleOnClick = async (provider: any) => {
+    const response = await socialMediaAuth(provider);
+    console.log(response);
+  };
+
   return (
     <>
+      <button onClick={() => handleOnClick(facebookProvider)}>facebook</button>
+      <button onClick={() => handleOnClick(githubProvider)}>github</button>
+      <button onClick={() => handleOnClick(googleProvider)}>google</button>
       <h1>APP</h1>
     </>
   );
