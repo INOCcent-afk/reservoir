cb3e4b5d0c31abdfafaee4d5b02acc7edc95fea1
Dave Inoc <michael828inoc@gmail.com>
2021-05-17 22:03:14 +0800

    added auth logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { githubProvider, googleProvider } from "./config/authMethod";
 import socialMediaAuth, { auth } from "./utils/auth";
-import { Route, Switch } from "react-router";
+import { Route, Switch, useHistory } from "react-router";
 
 import AppScreen from "./screens/AppSreen";
 import LogIn from "./screens/LogIn";
@@ -13,6 +13,8 @@ import { makeStyles, createStyles } from "@material-ui/core/styles";
 import { BodyInner, GlobalStyle } from "./assets/styles/GlobalStyle";
 import { Colors } from "./assets/styles/Colors";
 import MobileNavigation from "./containers/MobileNavigation";
+
+import firebase from "./config/firebase-config";
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -32,37 +34,55 @@ const useStyles = makeStyles(() =>
 );
 
 const App = () => {
+  const history = useHistory();
   const classes = useStyles();
-  const [status, setStatus] = useState(false);
+
   const [name, setName] = useState("");
   const [pPic, setPPic] = useState("");
+  const [currentUser, setCurrentUser] =
+    React.useState<firebase.User | null | boolean>(null);
 
   const handleOnClick = async (provider: any) => {
     const response = await socialMediaAuth(provider);
 
-    setPPic(`${response.photoURL}?access_token=`);
-    setName(response.displayName);
+    setPPic(response?.photoURL!);
+    setName(response?.displayName!);
 
-    setStatus(true);
+    history.push("/app");
   };
 
   const logOut = () => {
     auth
       .signOut()
       .then(() => {
-        setStatus(false);
         console.log("logout");
       })
       .catch((err) => {
         console.log(err.message);
       });
+    history.push("/");
   };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setPPic(user?.photoURL!);
+      setName(user?.displayName!);
+    });
+
+    if (currentUser === true) {
+      history.push("/app");
+    }
+    if (currentUser === false) {
+      history.push("/");
+    }
+  }, []);
 
   return (
     <>
       <GlobalStyle />
       <BodyInner>
-        {/* {status ? (
+        {currentUser ? (
           <>
             <h1 onClick={logOut}>logout</h1>
             <h1>{name}</h1>
@@ -77,7 +97,7 @@ const App = () => {
               google
             </button>
           </>
-        )} */}
+        )}
         {/* <div className={classes.root}>
           <Pagination count={5} color="primary"></Pagination>
         </div> */}
--- src/screens/AppSreen/AppScreen.tsx
+++ src/screens/AppSreen/AppScreen.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const AppScreen = () => {
-  return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default AppScreen;
